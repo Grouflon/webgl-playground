9ea8e2f3c6e274551a88706affc8b4794914d532
Remi laptop <remi@uselessworks.com>
2015-03-25 01:36:52 +0100

    webGL context set up

--- src/js/main.js
+++ src/js/main.js
@@ -8,5 +8,31 @@ define([
     ]
 	, function ()
     {
-        console.log("Hello");
+	    var gl;
+		var canvas = document.getElementById("glcanvas");
+	    initWebGL(canvas);
+
+	    if (gl)
+	    {
+		    gl.clearColor(0.0, 0.0, 0.0, 1.1);
+		    gl.enable(gl.DEPTH_TEST);
+		    gl.depthFunc(gl.LEQUAL);
+		    gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
+	    }
+
+
+	    function initWebGL(canvas) {
+		    gl = null;
+
+		    try
+		    {
+			    gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+		    }
+		    catch(e) {}
+
+		    if (!gl)
+		    {
+			    console.error("Impossible d'initialiser le WebGL. Il est possible que votre navigateur ne supporte pas cette fonctionnalité.");
+		    }
+	    }
 	});
